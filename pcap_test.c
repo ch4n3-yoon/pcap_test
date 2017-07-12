16bd615b98f365f0db48a1ba988fe7c8a9560763
chaneyoon <chaneyoon@gmail.com>
2017-07-13 00:28:39 +0900

    fix code to prevent Windows OS

--- pcap_test.c
+++ pcap_test.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+	#define isWindows 1
+#elif __linux__
+	#define isWindows 0
+#endif
+
+int main(int argc, char * argv[]) {
+
+
+	// prevent windows os
+	if(isWindows) {
+		printf("[*] This program doesn't support your OS.\n");
+		exit(-1);
+	} 
+
+
+
+
+}
