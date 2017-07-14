7837ea626a3b2a62372cba53ec7bdd70007bdf02
root <root@localhost.localdomain>
2017-07-15 01:56:33 +0900

    add function which is grabbing a packet

--- pcap_test.c
+++ pcap_test.c
@@ -17,12 +17,12 @@ int userPrevent(void);
 int main(int argc, char * argv[]) {
 
 
-	pcap_t *handle;					/* Session handle */
+	pcap_t *handle;				/* Session handle */
 	char * dev, errbuf[PCAP_ERRBUF_SIZE];
 	struct bpf_program fp;			/* The compiled filter expression */
-	char filter_exp[] = "port 80";	/* The filter expression */
-	bpf_u_int32 mask;				/* The netmask of our sniffing device */
-	bpf_u_int32 net;				/* The IP of our sniffing device */
+	char filter_exp[] = "port 80";		/* The filter expression */
+	bpf_u_int32 mask;			/* The netmask of our sniffing device */
+	bpf_u_int32 net;			/* The IP of our sniffing device */
 
 
 
@@ -52,16 +52,36 @@ int main(int argc, char * argv[]) {
 	printf("[*] Your network device : %s\n", dev);
 	
 	
-
-
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if(handle == NULL) {
 		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 		return 1;
 	}
 
+	// Compile and apply the filter 
+	if( pcap_compile(handle, &fp, filter_exp, 0, net) == -1 ) {
+		fprintf(stderr, "Couldn't parse filter %s: %s", filter_exp, pcap_geterr(handle));
+		return 1;
+	}
+	if( pcap_setfilter(handle, &fp) == -1 ) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return 1;
+	}
 
 
+
+	// Grab a packet
+	packet = pcap_next(handle, &header);
+
+	// print its length
+	printf("Jacked a packet with length of [%d]\n", header.len);
+	
+
+	// close the session
+	pcap_close(handle);
+
+	return 0;
+	
 
 }
 
